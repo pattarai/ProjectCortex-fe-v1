5b4f582116d8c9a234161dc680ea7d1bbcd15912
Daniel Mark <daniel.mark@stratforge.com>
2021-10-13 18:51:29 +0530

    Test

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@mui/material';
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 export function HomePage() {
+  var testVar;
   return (
     <>
       <Helmet>
@@ -9,6 +11,7 @@ export function HomePage() {
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
       <span>HomePage container</span>
+      <Button variant="contained">Contained</Button>
     </>
   );
 }
