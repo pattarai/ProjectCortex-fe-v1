65b57b4ae9866140ada8ef04497a64e0f4a56407
Joshuafrankle <joshuafrankle7@gmail.com>
2021-11-07 17:02:41 +0530

    Created UserManagement

---
+++ src/app/pages/UserManagement/index.tsx
@@ -0,0 +1,15 @@
+/**
+ *
+ * UserManagement
+ *
+ */
+import * as React from 'react';
+// import styled from 'styled-components/macro';
+
+interface Props {}
+
+export function UserManagement(props: Props) {
+  return <></>;
+}
+
+// const Div = styled.div``;
