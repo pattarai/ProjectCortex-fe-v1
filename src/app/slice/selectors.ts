3dd98ef75953d0a245927c6de9f888f353ecff2c
Joshuafrankle <joshuafrankle7@gmail.com>
2021-11-21 17:41:00 +0530

    Initial state first

--- src/app/slice/selectors.ts
+++ src/app/slice/selectors.ts
@@ -3,7 +3,7 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from 'types';
 import { initialState } from '.';
 
-const selectSlice = (state: RootState) => state.userManagement || initialState;
+const selectSlice = (state: RootState) => initialState || state.userManagement;
 
 export const selectUserManagement = createSelector(
   [selectSlice],
