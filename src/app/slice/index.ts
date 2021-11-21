e9b4358687ce52a29afcab5cc145b6028c93f2ce
Joshuafrankle <joshuafrankle7@gmail.com>
2021-11-21 15:50:10 +0530

    Created slice and saga

---
+++ src/app/slice/index.ts
@@ -0,0 +1,35 @@
+import { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from 'utils/@reduxjs/toolkit';
+import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors';
+import { userManagementSaga } from './saga';
+import { UserManagementState } from './types';
+
+export const initialState: UserManagementState = {};
+
+const slice = createSlice({
+  name: 'userManagement',
+  initialState,
+  reducers: {
+    someAction(state, action: PayloadAction<any>) {},
+  },
+});
+
+export const { actions: userManagementActions } = slice;
+
+export const useUserManagementSlice = () => {
+  useInjectReducer({ key: slice.name, reducer: slice.reducer });
+  useInjectSaga({ key: slice.name, saga: userManagementSaga });
+  return { actions: slice.actions };
+};
+
+/**
+ * Example Usage:
+ *
+ * export function MyComponentNeedingThisSlice() {
+ *  const { actions } = useUserManagementSlice();
+ *
+ *  const onButtonClick = (evt) => {
+ *    dispatch(actions.someAction());
+ *   };
+ * }
+ */
