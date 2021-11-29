6461843c876a60092a465f8aa1dfd31cd9af2474
Joshuafrankle <joshuafrankle7@gmail.com>
2021-11-29 19:13:41 +0530

    Changed modal to dialog

--- src/app/pages/UserManagement/Popup.tsx
+++ src/app/pages/UserManagement/Popup.tsx
@@ -1,17 +1,10 @@
-import * as React from 'react';
-import { Box, Backdrop, Modal, IconButton } from '@mui/material';
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  IconButton,
+} from '@mui/material';
 import { MdOutlineCancel } from 'react-icons/md';
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: 4,
-};
 
 export default function TransitionsModal({
   title,
@@ -21,35 +14,28 @@ export default function TransitionsModal({
 }) {
   return (
     <>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
+      <Dialog
         open={openModal}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
       >
-        <Box
-          sx={style}
-          style={{ width: '95%', height: '95%', overflowX: 'auto' }}
-          className="w-md-50 h-md-60 d-md-flex flex-column justify-content-center"
-        >
-          <div className="d-flex justify-content-between align-items-center mb-4">
-            <h1>{title}</h1>
-            <IconButton
-              color="error"
-              aria-label="close"
-              component="span"
-              onClick={() => setOpenModal(false)}
-            >
-              <MdOutlineCancel />
-            </IconButton>
-          </div>
-          {children}
-        </Box>
-      </Modal>
+        <div className="d-flex justify-content-between align-items-center m-3 mx-4">
+          <h1>{title}</h1>
+          <IconButton
+            color="error"
+            aria-label="close"
+            component="span"
+            onClick={() => setOpenModal(false)}
+          >
+            <MdOutlineCancel />
+          </IconButton>
+        </div>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {children}
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
