75671a448f3b87e9c1650e16d0e66f99caec4870
Joshuafrankle <joshuafrankle7@gmail.com>
2021-12-05 11:15:45 +0530

    Dialog UI changes

--- src/app/pages/UserManagement/Popup.tsx
+++ src/app/pages/UserManagement/Popup.tsx
@@ -1,9 +1,4 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogContentText,
-  IconButton,
-} from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, IconButton } from '@mui/material';
 import { MdOutlineCancel } from 'react-icons/md';
 
 export default function TransitionsModal({
@@ -19,22 +14,20 @@ export default function TransitionsModal({
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <div className="d-flex justify-content-between align-items-center m-3 mx-4">
-          <h1>{title}</h1>
-          <IconButton
-            color="error"
-            aria-label="close"
-            component="span"
-            onClick={() => setOpenModal(false)}
-          >
-            <MdOutlineCancel />
-          </IconButton>
-        </div>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {children}
-          </DialogContentText>
-        </DialogContent>
+        <DialogTitle id="alert-dialog-title">
+          <div className="d-flex justify-content-between align-items-center my-3">
+            <h2>{title}</h2>
+            <IconButton
+              color="error"
+              aria-label="close"
+              component="span"
+              onClick={() => setOpenModal(false)}
+            >
+              <MdOutlineCancel />
+            </IconButton>
+          </div>
+        </DialogTitle>
+        <DialogContent>{children}</DialogContent>
       </Dialog>
     </>
   );
