d8e26d9cf54fae98466c47731b12bdff599caebc
Joshuafrankle <joshuafrankle7@gmail.com>
2021-12-08 20:50:49 +0530

    custom breakpoints

--- src/css/MUIThemes.tsx
+++ src/css/MUIThemes.tsx
@@ -1,6 +1,15 @@
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+    },
+  },
   palette: {
     primary: {
       light: '#3c44b126',
