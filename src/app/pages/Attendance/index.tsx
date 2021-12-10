62866d6b0fb4f7805a246f22e30f311cf24d85b4
Joshuafrankle <joshuafrankle7@gmail.com>
2021-12-10 16:53:47 +0530

    Update Data Grid

--- src/app/pages/Attendance/index.tsx
+++ src/app/pages/Attendance/index.tsx
@@ -11,57 +11,40 @@ import {
   FormControl,
   Select,
   TextField,
+  Card,
 } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
-import { SelectChangeEvent } from '@mui/material/Select';
 import { DataGrid } from '@mui/x-data-grid';
 
 import Popup from '../../components/Popup';
 import MemberForm from './MemberForm';
 
+import { useSelector } from 'react-redux';
+import { useAttendanceSlice } from './slice';
+import { selectAttendance } from './slice/selectors';
+import { MemberAttendanceType } from './slice/types';
+
 interface Props {}
 
-type AttendanceType = {
-  eventName: string;
-  eventDate: Date | null;
-  eventType: string;
-};
-
 export function Attendance(props: Props) {
-  const [openPopup, setOpenPopup] = useState(false);
-  const eventsList = ['Event 1', 'Event 2', 'Event 3'];
+  const { actions } = useAttendanceSlice();
+  const user = useSelector(selectAttendance);
 
-  const [value, setValue] = useState<AttendanceType>({
+  const eventsList = ['Think Tank', 'Elevate'];
+  const [openPopup, setOpenPopup] = useState(false);
+
+  const [value, setValue] = useState({
     eventName: '',
     eventType: '',
-    eventDate: null,
+    eventDate: '',
   });
 
-  const handleChange = (event: SelectChangeEvent) => {
-    if (event.target.name === 'eventType') {
-      setValue({
-        ...value,
-        eventType: event.target.value,
-      });
-    }
-    if (event.target.name === 'eventName') {
-      setValue({
-        ...value,
-        eventName: event.target.value,
-      });
-    }
-  };
-
-  const [rows, setRows] = useState([
-    { id: 1, sno: 1, name: 'Joshua', status: 'Present' },
-    { id: 2, sno: 2, name: 'Jeinthan', status: 'Absent' },
-    { id: 3, sno: 3, name: 'Dhivya', status: 'Informed' },
-  ]);
+  const [rows, setRows] = useState<null | MemberAttendanceType[]>(null);
 
   const columns = [
-    { field: 'sno', headerName: 'S.No.', width: 400 },
+    { field: 'id', headerName: 'S.No.', width: 400 },
     { field: 'name', headerName: 'Name', width: 400 },
     {
       field: 'status',
@@ -82,71 +65,111 @@ export function Attendance(props: Props) {
     );
   }
 
+  function handleSubmit() {
+    user.forEach(item => {
+      if (
+        item.eventDate === value.eventDate &&
+        item.eventName === value.eventName
+      ) {
+        const userData = user[user.indexOf(item)].members;
+        setRows(userData);
+      }
+    });
+  }
+
   return (
     <>
-      <div className="py-3 px-4 d-md-flex">
-        <FormControl fullWidth sx={{ mr: '20px' }}>
-          <InputLabel id="demo-simple-select-label">Event Name</InputLabel>
-          <Select
-            name="eventName"
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={value.eventName}
-            label="Event Name"
-            onChange={handleChange}
-          >
-            {eventsList.map(event => (
-              <MenuItem key={event} value={event}>
-                {event}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl
-          fullWidth
-          sx={{ mx: { md: '20px' }, my: { xs: '20px', md: '0px' } }}
+      <div className="my-3 mx-3 mx-md-5 d-flex flex-column align-justify-center vh-100">
+        <Card
+          className="d-flex flex-column align-justify-center p-3 p-md-5"
+          sx={{ width: '100%', height: '100%' }}
         >
-          <InputLabel id="demo-simple-select-label">Event Type</InputLabel>
-          <Select
-            name="eventType"
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={value.eventType}
-            label="Event type"
-            onChange={handleChange}
-          >
-            <MenuItem value="crew">Crew</MenuItem>
-            <MenuItem value="learnzeit">Learnzeit</MenuItem>
-            <MenuItem value="external">External</MenuItem>
-          </Select>
-        </FormControl>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <DatePicker
-            label="Date"
-            value={value.eventDate}
-            onChange={newValue => {
-              setValue({ ...value, eventDate: newValue });
-            }}
-            renderInput={params => (
-              <TextField
-                {...params}
-                sx={{ width: '100%', mx: { md: '20px' } }}
+          <div className="pb-3 d-md-flex w-md-100">
+            <FormControl fullWidth sx={{ mr: '20px' }}>
+              <InputLabel id="demo-simple-select-label">Event Name</InputLabel>
+              <Select
+                name="eventName"
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={value.eventName}
+                label="Event Name"
+                onChange={event =>
+                  setValue({
+                    ...value,
+                    eventName: event.target.value,
+                  })
+                }
+              >
+                {eventsList.map(event => (
+                  <MenuItem key={event} value={event}>
+                    {event}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl
+              fullWidth
+              sx={{ mx: { md: '20px' }, my: { xs: '20px', md: '0px' } }}
+            >
+              <InputLabel id="demo-simple-select-label">Event Type</InputLabel>
+              <Select
+                name="eventType"
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={value.eventType}
+                label="Event type"
+                onChange={event =>
+                  setValue({
+                    ...value,
+                    eventType: event.target.value,
+                  })
+                }
+              >
+                <MenuItem value="crew">Crew</MenuItem>
+                <MenuItem value="learnzeit">Learnzeit</MenuItem>
+                <MenuItem value="external">External</MenuItem>
+              </Select>
+            </FormControl>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Date"
+                value={value.eventDate === '' ? null : value.eventDate}
+                onChange={newValue => {
+                  const date = new Date(`${newValue}`).toDateString();
+                  setValue({ ...value, eventDate: date });
+                }}
+                renderInput={params => (
+                  <TextField
+                    {...params}
+                    sx={{ width: '100%', mx: { md: '20px' } }}
+                  />
+                )}
               />
-            )}
-          />
-        </LocalizationProvider>
-        <Button variant="contained" sx={{ width: '50%' }}>
-          Submit
-        </Button>
-      </div>
-      <div className="mt-3 px-4" style={{ height: 400, width: '100%' }}>
-        <DataGrid
-          components={{
-            Toolbar: EditToolbar,
-          }}
-          rows={rows}
-          columns={columns}
-        />
+            </LocalizationProvider>
+            <Button
+              variant="contained"
+              sx={{
+                width: { xs: '100%', md: '50%' },
+                mt: { xs: '20px', md: '0px' },
+                mx: { xs: '0px', md: '20px' },
+              }}
+              onClick={handleSubmit}
+            >
+              Submit
+            </Button>
+          </div>
+          {rows !== null && (
+            <div className="mt-3" style={{ height: 400, width: '100%' }}>
+              <DataGrid
+                components={{
+                  Toolbar: EditToolbar,
+                }}
+                rows={rows}
+                columns={columns}
+              />
+            </div>
+          )}
+        </Card>
       </div>
       <Popup
         title="Add Member"
