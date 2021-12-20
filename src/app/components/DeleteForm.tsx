43d7f1953224d1868a5102286cdb6dfd18944c18
Joshuafrankle <joshuafrankle7@gmail.com>
2021-12-20 10:58:13 +0530

    Update DeleteForm

--- src/app/components/DeleteForm.tsx
+++ src/app/components/DeleteForm.tsx
@@ -1,26 +1,35 @@
-import { Button } from '@mui/material';
+import { useState } from 'react';
+import { Button, CircularProgress } from '@mui/material';
 import { useDispatch } from 'react-redux';
 
 export default function DeleteForm({ setOpenModal, action }) {
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   return (
     <>
-      <Button
-        variant="outlined"
-        onClick={() => {
-          dispatch(action);
-          setOpenModal(false);
-        }}
-      >
-        yes
-      </Button>
-      <Button
-        variant="outlined"
-        className="ms-2"
-        onClick={() => setOpenModal(false)}
-      >
-        No
-      </Button>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Button
+            variant="outlined"
+            onClick={async () => {
+              setLoading(true);
+              await dispatch(action);
+              setOpenModal(false);
+            }}
+          >
+            yes
+          </Button>
+          <Button
+            variant="outlined"
+            className="ms-2"
+            onClick={() => setOpenModal(false)}
+          >
+            No
+          </Button>
+        </>
+      )}
     </>
   );
 }
