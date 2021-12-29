a8092415f2abb2e81fe649eea49e50767824158b
Joshuafrankle <joshuafrankle7@gmail.com>
2021-12-29 10:45:15 +0530

    Created getUser saga

--- src/app/pages/UserManagement/slice/saga.ts
+++ src/app/pages/UserManagement/slice/saga.ts
@@ -1,8 +1,17 @@
-// import { take, call, put, select, takeLatest } from 'redux-saga/effects';
-// import { userManagementActions as actions } from '.';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { userManagementActions as actions } from '.';
+import { axiosGet } from '../../../requests';
 
-// function* doSomething() {}
+function* handleGetUser() {
+  try {
+    const res = yield call(() => axiosGet('/usermanagement'));
+    const data = res.data.data;
+    yield put(actions.setUser(data));
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export function* userManagementSaga() {
-  // yield takeLatest(actions.someAction.type, doSomething);
+  yield takeLatest(actions.getUser.type, handleGetUser);
 }
