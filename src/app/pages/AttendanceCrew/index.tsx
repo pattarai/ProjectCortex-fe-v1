0f05edd52ea708a0f30cf769b67a52084b9df9f8
dhivya910 <dhivyadharshni910@gmail.com>
2022-02-01 20:23:30 +0530

    progress bar in card

--- src/app/pages/AttendanceCrew/index.tsx
+++ src/app/pages/AttendanceCrew/index.tsx
@@ -4,6 +4,7 @@
  *
  */
 import * as React from 'react';
+import Card from '@mui/material/Card';
 import CircularProgress, {
   CircularProgressProps,
 } from '@mui/material/CircularProgress';
@@ -15,39 +16,47 @@ interface Props {}
 export function AttendanceCrew(props: Props) {
   return (
     <>
-      <div className=" d-flex align-justify-center">
-        <div>
-          <CircularProgress
-            variant="determinate"
-            disableShrink
-            size={100}
-            thickness={4}
-            value={33}
-            {...props}
-          />
-        </div>
-        <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-          <CircularProgress variant="determinate" {...props} />
-          <Box
-            sx={{
-              top: 0,
-              left: 0,
-              bottom: 0,
-              right: 0,
-              position: 'absolute',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Typography
-              variant="caption"
-              component="div"
-              color="text.secondary"
-            />
-          </Box>
-        </Box>
-      </div>
+      <Box sx={{ minWidth: 275 }}>
+        <Card
+          sx={{
+            maxWidth: '25%',
+          }}
+        >
+          <div className=" d-flex align-justify-center">
+            <div>
+              <CircularProgress
+                variant="determinate"
+                disableShrink
+                size={100}
+                thickness={4}
+                value={33}
+                {...props}
+              />
+            </div>
+            <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+              <CircularProgress variant="determinate" {...props} />
+              <Box
+                sx={{
+                  top: 0,
+                  left: 0,
+                  bottom: 0,
+                  right: 0,
+                  position: 'absolute',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Typography
+                  variant="caption"
+                  component="div"
+                  color="text.secondary"
+                />
+              </Box>
+            </Box>
+          </div>
+        </Card>
+      </Box>
     </>
   );
 }
