061766dd488c2e87fe14a2840980725f4f75bb03
Joshuafrankle <joshuafrankle7@gmail.com>
2022-02-27 19:06:39 +0530

    Update member form

--- src/app/pages/Attendance/MemberForm.tsx
+++ src/app/pages/Attendance/MemberForm.tsx
@@ -1,51 +1,100 @@
 import { useState } from 'react';
-import { Button, TextField } from '@mui/material';
+import {
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@mui/material';
 import { useDispatch } from 'react-redux';
 
-export default function MemberForm({ currentEventId, setOpenModal, actions }) {
+export default function MemberForm({
+  currentEventId,
+  setOpenModal,
+  actions,
+  updateMember,
+  crewAttendance,
+}) {
   const dispatch = useDispatch();
 
-  const [memberName, setMemberName] = useState('');
+  const [member, setMember] = useState({
+    memberName: '',
+    memberStatus: 1,
+  });
+  let updateMemberName = '';
 
   const [errors, setErrors] = useState({
     memberNameError: '',
     isError: false,
   });
 
-  function handleSubmit() {
-    let err = { ...errors };
-    if (memberName.trim() === '') {
-      err.memberNameError = 'Name is required';
-      err.isError = true;
-      setErrors(err);
-    } else {
+  if (updateMember) {
+    const member = crewAttendance.find(
+      member => member.userId === updateMember,
+    );
+    updateMemberName = `${member.users.firstName} ${member.users.lastName}`;
+  }
+
+  function handleUpdateOrSubmit() {
+    if (updateMember) {
       dispatch(
-        actions.addExternalMember({
-          eventId: currentEventId,
-          name: memberName,
+        actions.updateCrewMember({
+          userId: updateMember,
+          status: member.memberStatus,
         }),
       );
-      setOpenModal(false);
+    } else {
+      let err = { ...errors };
+      if (member.memberName.trim() === '') {
+        err.memberNameError = 'Name is required';
+        err.isError = true;
+        setErrors(err);
+      } else {
+        dispatch(
+          actions.addExternalMember({
+            eventId: currentEventId,
+            name: member.memberName,
+          }),
+        );
+      }
     }
+    setOpenModal(false);
   }
 
   return (
-    <div>
+    <>
       <TextField
+        disabled={updateMember ? true : false}
         id="outlined-basic"
-        error={errors.isError && (memberName.trim() === '' ? true : false)}
-        helperText={
-          errors.isError &&
-          (errors.memberNameError !== '' ? errors.memberNameError : '')
-        }
+        value={updateMember ? updateMemberName : member.memberName}
         label="Name"
         variant="outlined"
-        sx={{ width: '100%' }}
-        onChange={e => setMemberName(e.target.value)}
+        sx={{ width: '100%', mb: '1rem' }}
       />
-      <Button onClick={handleSubmit} variant="outlined" className="mt-2">
-        Add
+      {updateMember && (
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Status</InputLabel>
+          <Select
+            name="role"
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={member.memberStatus.toString()}
+            label="Roles"
+            onChange={e =>
+              setMember({ ...member, memberStatus: parseInt(e.target.value) })
+            }
+            className="mb-3"
+          >
+            <MenuItem value={0}>Absent</MenuItem>
+            <MenuItem value={1}>Present</MenuItem>
+            <MenuItem value={2}>Informed</MenuItem>
+          </Select>
+        </FormControl>
+      )}
+      <Button onClick={handleUpdateOrSubmit} variant="outlined" className="">
+        {updateMember ? 'Update' : 'Add'}
       </Button>
-    </div>
+    </>
   );
 }
