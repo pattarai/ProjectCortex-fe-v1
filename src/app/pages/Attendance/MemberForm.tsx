5710261d200ed8b30e13487a7fba865ae11c24ba
Joshuafrankle <joshuafrankle7@gmail.com>
2022-02-27 10:34:23 +0530

    Member form update

--- src/app/pages/Attendance/MemberForm.tsx
+++ src/app/pages/Attendance/MemberForm.tsx
@@ -1,38 +1,30 @@
 import { useState } from 'react';
-import {
-  InputLabel,
-  MenuItem,
-  FormControl,
-  Select,
-  TextField,
-} from '@mui/material';
-import LoadingButton from '@mui/lab/LoadingButton';
+import { Button, TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
 
 export default function MemberForm({ currentEventId, setOpenModal, actions }) {
-  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
 
-  const [member, setMember] = useState({
-    eventId: currentEventId,
-    name: '',
-    status: 0,
-  });
+  const [memberName, setMemberName] = useState('');
 
   const [errors, setErrors] = useState({
-    nameError: '',
+    memberNameError: '',
     isError: false,
   });
 
-  async function handleSubmit() {
+  function handleSubmit() {
     let err = { ...errors };
-    if (member.name.trim() === '') {
-      err.nameError = 'Name is required';
+    if (memberName.trim() === '') {
+      err.memberNameError = 'Name is required';
       err.isError = true;
       setErrors(err);
     } else {
-      setLoading(true);
-      await dispatch(actions.addUser(member));
+      dispatch(
+        actions.addExternalMember({
+          eventId: currentEventId,
+          name: memberName,
+        }),
+      );
       setOpenModal(false);
     }
   }
@@ -41,39 +33,19 @@ export default function MemberForm({ currentEventId, setOpenModal, actions }) {
     <div>
       <TextField
         id="outlined-basic"
-        error={errors.isError && (member.name.trim() === '' ? true : false)}
+        error={errors.isError && (memberName.trim() === '' ? true : false)}
         helperText={
-          errors.isError && (errors.nameError !== '' ? errors.nameError : '')
+          errors.isError &&
+          (errors.memberNameError !== '' ? errors.memberNameError : '')
         }
         label="Name"
         variant="outlined"
         sx={{ width: '100%' }}
-        onChange={e => setMember({ ...member, name: e.target.value })}
+        onChange={e => setMemberName(e.target.value)}
       />
-      <FormControl fullWidth sx={{ my: '20px' }}>
-        <InputLabel id="demo-simple-select-label">Status</InputLabel>
-        <Select
-          value={member.status.toString()}
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          label="Status"
-          onChange={e =>
-            setMember({ ...member, status: parseInt(e.target.value) })
-          }
-        >
-          <MenuItem value={1}>Present</MenuItem>
-          <MenuItem value={0}>Absent</MenuItem>
-          <MenuItem value={2}>Informed</MenuItem>
-        </Select>
-      </FormControl>
-      <LoadingButton
-        loading={loading}
-        variant="outlined"
-        sx={{ width: '100%' }}
-        onClick={handleSubmit}
-      >
-        Add Member
-      </LoadingButton>
+      <Button onClick={handleSubmit} variant="outlined" className="mt-2">
+        Add
+      </Button>
     </div>
   );
 }
