44954b6c20468d0bbc9cf2b533e360e80d6c9132
RAKSHA V G <vgraksha26@gmail.com>
2022-03-02 22:22:43 +0530

    list to table view

--- src/app/pages/RankingCrew/MemberScoreCard.tsx
+++ src/app/pages/RankingCrew/MemberScoreCard.tsx
@@ -6,11 +6,11 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import List from '@mui/material/List';
-import ListSubheader from '@mui/material/ListSubheader';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import { Divider } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import { axiosPost } from '../../requests';
 
 type userDetails = {
@@ -31,7 +31,7 @@ export default function MemberScoreCard() {
   const [userData, setUserData] = useState<ScoreData | null>(null);
   const [currentPhase, setCurrentPhase] = useState<null | userDetails[]>(null);
 
-  function onlyUnique(value, index, self) {
+  function onlyUnique(value: any, index: any, self: any) {
     return self.indexOf(value) === index;
   }
   const phases = userData
@@ -63,91 +63,77 @@ export default function MemberScoreCard() {
     <>
       <div className="container my-4">
         <div className="row">
-          <div className="col-12 col-md-5"></div>
-          <div className="col-12 col-md-7">
-            <Card
-              elevation={2}
-              sx={{
-                textAlign: 'center',
-              }}
-            >
-              <CardContent sx={{ flex: 1 }}>
-                <div className="d-flex justify-content-end">
-                  <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">
-                      Select Phase
-                    </InputLabel>
-                    <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      value={
-                        currentPhase
-                          ? currentPhase[0].factors.phase.toString()
-                          : ''
-                      }
-                      label="Phase"
-                      onChange={handleChange}
-                    >
-                      {unique?.map((data, keyy) => (
-                        <MenuItem key={keyy} value={data}>
-                          {`Phase  ${data}`}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </div>
-                <List>
-                  <ListSubheader>
-                    <div className="d-flex justify-content-between">
-                      <span>SNO</span>
-                      <span>CONTRIBUTION</span>
-                      <span>MAX SCORE</span>
-                      <span>SCORE</span>
-                    </div>
-                  </ListSubheader>
-                  {currentPhase &&
-                    currentPhase.map((list, index) => (
-                      <ListItem
-                        key={`${list}-${index}`}
-                        className="d-flex align-items-center justify-content-between"
-                      >
-                        <ListItemText
-                          primary={`#${index + 1}`}
-                          className="d-flex justify-content-start"
-                        />
-                        <ListItemText
-                          primary={list.factors.factorName}
-                          className="d-flex justify-content-start"
-                        />
-                        <ListItemText
-                          primary={list.factors.maxScore}
-                          className="d-flex justify-content-end"
-                        />
-                        <ListItemText
-                          primary={list.score}
-                          className="d-flex justify-content-end"
-                        />
-                      </ListItem>
+          <div className="col-12">
+            <Card elevation={2}>
+              <CardContent>
+                <FormControl fullWidth>
+                  <InputLabel>Select Phase</InputLabel>
+                  <Select
+                    value={
+                      currentPhase
+                        ? currentPhase[0].factors.phase.toString()
+                        : ''
+                    }
+                    label="Select Phase"
+                    onChange={handleChange}
+                  >
+                    {unique?.map((data, keyy) => (
+                      <MenuItem key={keyy} value={data}>
+                        {`Phase  ${data}`}
+                      </MenuItem>
                     ))}
-                </List>
-                <Divider />{' '}
-                {userData &&
-                  userData.totalScoreByPhase.map((list, index) => (
-                    <List key={`${list}-${index}`}>
-                      <ListItem>
-                        <ListItemText />
-                        <ListItemText
-                          primary="TOTAL"
-                          className="d-flex justify-content-start"
-                        />
-                        <ListItemText />
-                        <ListItemText
-                          primary={list}
-                          className="d-flex justify-content-end"
-                        />
-                      </ListItem>
-                    </List>
-                  ))}
+                  </Select>
+                </FormControl>
+                <Table className="mt-2">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>SNO</TableCell>
+                      <TableCell align="left">CONTRIBUTION</TableCell>
+                      <TableCell align="right">MAXSCORE</TableCell>
+                      <TableCell align="right">SCORE</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {currentPhase &&
+                      currentPhase.map((list, index) => (
+                        <TableRow
+                          key={`${list}-${index}`}
+                          sx={{
+                            '&:last-child td, &:last-child th': {
+                              border: 0,
+                            },
+                          }}
+                        >
+                          <TableCell component="th" scope="row">
+                            {`#${index + 1}`}
+                          </TableCell>
+                          <TableCell align="left">
+                            {list.factors.factorName}
+                          </TableCell>
+                          <TableCell align="right">
+                            {list.factors.maxScore}
+                          </TableCell>
+                          <TableCell align="right">{list.score}</TableCell>
+                        </TableRow>
+                      ))}
+                    {userData &&
+                      userData.totalScoreByPhase.map((list, index) => (
+                        <TableRow
+                          key={`${list}-${index}`}
+                          sx={{
+                            '&:last-child td, &:last-child th': {
+                              border: 0,
+                            },
+                          }}
+                        >
+                          <TableCell />
+                          <TableCell />
+                          <TableCell />
+                          <TableCell align="right">{list}</TableCell>
+                        </TableRow>
+                      ))}
+                  </TableBody>
+                </Table>
               </CardContent>
             </Card>
           </div>
