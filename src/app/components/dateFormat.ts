1ac63350f6e9bdd4d280f735c722191b4009ceae
Joshuafrankle <joshuafrankle7@gmail.com>
2022-03-03 12:23:18 +0530

    Added components

---
+++ src/app/components/dateFormat.ts
@@ -0,0 +1,24 @@
+export function dateFormat(dat: string | null) {
+  let date = new Date(`${dat}`);
+  let currentDate: string | number = date.getDate();
+  let currentMonth: string | number = date.getMonth() + 1;
+  let currentYear = date.getFullYear();
+  if (currentDate < 10) {
+    currentDate = `0${currentDate}`;
+  }
+  if (currentMonth < 10) {
+    currentMonth = `0${currentMonth}`;
+  }
+  return `${currentMonth}/${currentDate}/${currentYear}`;
+}
+
+export function randomString() {
+  let characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 8; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
