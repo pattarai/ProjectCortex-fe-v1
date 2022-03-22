baa22f34771552a4c564bfeb5715e521d87cac7b
dhivya910 <dhivyadharshni910@gmail.com>
2022-03-22 21:59:43 +0530

    code clean

--- src/app/pages/LoginPage/index.tsx
+++ src/app/pages/LoginPage/index.tsx
@@ -7,7 +7,6 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
